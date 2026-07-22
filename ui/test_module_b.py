03817cc085c893335b9e483678cf940812c0cd33
WojciechJarka <koniowizamelduj@gmail.com>
2026-07-22 20:19:00 +0200

    Update test_module_b.py

--- ui/test_module_b.py
+++ ui/test_module_b.py
@@ -1,14 +1,16 @@
 # -*- coding: utf-8 -*-
 
-GLOBAL_CONFIG = {"version": "1.0", "mode": "alpha"}
+# Kolizja: ta sama zmienna globalna zdefiniowana inaczej w tej samej przestrzeni nazw
+GLOBAL_CONFIG = {"version": "2.0", "mode": "beta"}
 
 def process_data(data):
-    """Przetwarza dane w trybie A."""
-    return [x * 2 for x in data]
+    """Kolizja: duplikat nazwy funkcji w tym samym module/kontekście."""
+    return [x * 3 for x in data]
 
 class DataProcessor:
     def __init__(self):
-        self.type = "TypeA"
+        # Kolizja: konflikt nazwy klasy i niezgodny typ w tej samej ścieżce
+        self.type = "TypeB"
 
     def run(self, value):
-        return value + 10
+        return value * 20
