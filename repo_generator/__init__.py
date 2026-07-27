67009677bda6643ba3166533c20926d79d4b5c26
WojciechJarka <koniowizamelduj@gmail.com>
2026-07-27 19:41:06 +0200

    Create __init__.py

---
+++ repo_generator/__init__.py
@@ -0,0 +1,11 @@
+# Repo_Guardian/repo_generator/__init__.py
+# ============================================================
+# Repo Guardian - Repo Generator package
+# ============================================================
+
+from .repo_gui import RepoGenerator, run_repo_generator
+
+__all__ = [
+    "RepoGenerator",
+    "run_repo_generator",
+]
